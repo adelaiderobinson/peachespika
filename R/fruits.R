bea0d7ef3edc9fef1a530a4dfa823743bc63246d
adelaiderobinson <adelaide.robinson445@gmail.com>
2022-08-12 11:36:44 -0700

    Initial commit

---
+++ R/fruits.R
@@ -0,0 +1,13 @@
+#' Title
+#'
+#' @param fruit
+#' @param number
+#'
+#' @return
+#' @export
+#'
+#' @examples fruits("apples", 12)
+fruits <- function(fruit,number){
+  print(paste("I have", number, fruit))
+}
+
---
+++ R/snacks.R
@@ -0,0 +1,13 @@
+#' Title
+#'
+#' @param grapes mass of grapes
+#' @param apples mass of apples
+#' @param burritos how many burritos I eat
+#'
+#' @return
+#' @export
+#'
+#' @examples snacks(grapes = 2.5, apples = 6, burritos = 1)
+snacks <- function(grapes, apples, burritos) {
+  (grapes + apples)/ burritos
+(grapes + apples)/ burritos }
